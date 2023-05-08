a037fc868e024a0f832eafb7293fdc60c2943b5b
rafaelabreuu <131624539+rafaelabreuu@users.noreply.github.com>
2023-05-08 00:24:34 -0300

    Update questao3.c

--- questao3.c
+++ questao3.c
@@ -1,79 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-typedef struct Caixa {
+typedef struct no{
     int peso;
-    struct Caixa* prox;
-} Caixa;
+    struct no * prox;
+}NO;
 
-typedef struct Pilha {
-    Caixa* topo;
-} Pilha;
+NO * pilhaA = NULL;
+NO * pilhaB = NULL;
+NO * pilhaC = NULL;
 
-Pilha* criar_pilha() {
-    Pilha* pilha = (Pilha*)malloc(sizeof(Pilha));
-    pilha->topo = NULL;
-    return pilha;
+void push(NO **pilha, int peso){
+    NO * novo = malloc(sizeof(NO));
+    novo->peso = peso;
+    novo->prox = *pilha;
+    *pilha = novo;
 }
 
-int pilha_vazia(Pilha* pilha) {
-    return pilha->topo == NULL;
-}
-
-void empilhar(Pilha* pilha, int peso) {
-    Caixa* nova_caixa = (Caixa*)malloc(sizeof(Caixa));
-    nova_caixa->peso = peso;
-    nova_caixa->prox = pilha->topo;
-    pilha->topo = nova_caixa;
-}
-
-int desempilhar(Pilha* pilha) {
-    if (pilha_vazia(pilha)) {
-        printf("Erro: Pilha vazia\n");
+int pop(NO **pilha){
+    if(*pilha == NULL){
+        printf("Pilha vazia!\n");
         return -1;
-    }
-    int peso = pilha->topo->peso;
-    Caixa* caixa_removida = pilha->topo;
-    pilha->topo = pilha->topo->prox;
-    free(caixa_removida);
-    return peso;
-}
-
-void imprimir_pilha(Pilha* pilha) {
-    printf("[ ");
-    Caixa* caixa_atual = pilha->topo;
-    while (caixa_atual != NULL) {
-        printf("%d ", caixa_atual->peso);
-        caixa_atual = caixa_atual->prox;
-    }
-    printf("]\n");
-}
-
-void mover_caixas(Pilha* pilha_origem, Pilha* pilha_auxiliar_1, Pilha* pilha_auxiliar_2) {
-    while (!pilha_vazia(pilha_origem)) {
-        int peso = desempilhar(pilha_origem);
-        if (peso == -1) {
-            printf("Erro ao desempilhar\n");
-            exit(1);
-        }
-        if (peso > pilha_auxiliar_1->topo->peso) {
-            empilhar(pilha_auxiliar_2, peso);
-            mover_caixas(pilha_auxiliar_1, pilha_origem, pilha_auxiliar_2);
-        } else {
-            empilhar(pilha_auxiliar_1, peso);
-        }
+    }else{
+        NO * temp = *pilha;
+        int valor = temp->peso;
+        *pilha = (*pilha)->prox;
+        free(temp);
+        return valor;
     }
 }
 
-void voltar_caixas(Pilha* pilha_destino, Pilha* pilha_auxiliar_1, Pilha* pilha_auxiliar_2) {
+void moverCaixas(NO **origem, NO **destino){
+    while(*origem != NULL){
+        int peso = pop(origem);
+        push(destino, peso);
+    }
+}
 
-    while (!pilha_vazia(pilha_auxiliar_1)) {
+int main(){
+    push(&pilhaA, 5);
+    push(&pilhaA, 3);
+    push(&pilhaA, 7);
+    push(&pilhaA, 5);
+    push(&pilhaA, 3);
+    push(&pilhaA, 3);
+    push(&pilhaA, 7);
     
-        int peso = desempilhar(pilha_auxiliar_1);
-        if (peso == -1) {
-            printf("Erro ao desempilhar\n");
-            exit(1);
+    while(pilhaA != NULL){
+        int peso = pop(&pilhaA);
+        if(peso > 7){
+            moverCaixas(&pilhaA, &pilhaB);
+            moverCaixas(&pilhaC, &pilhaA);
+            push(&pilhaA, peso);
+        }else if(peso > 5){
+            moverCaixas(&pilhaA, &pilhaC);
+            moverCaixas(&pilhaB, &pilhaA);
+            push(&pilhaA, peso);
+        }else{
+            push(&pilhaA, peso);
         }
-        empilhar(pilha_destino, peso);
     }
+    
+    printf("Caixas empilhadas com sucesso na pilha A!\n");
+    return 0;
 }
