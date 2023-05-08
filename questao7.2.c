38787d4414fa34084d86ecbb2dc81f99a7252c4d
rafaelabreuu <131624539+rafaelabreuu@users.noreply.github.com>
2023-05-07 23:37:41 -0300

    Update questao7.2.c

--- questao7.2.c
+++ questao7.2.c
@@ -1,66 +1,102 @@
-#define MAX_QUEUE_SIZE 100
+#include<stdio.h>
+#include<stdlib.h>
 
-typedef struct {
-    int frente, tras;
-    int dados[MAX_QUEUE_SIZE];
-} Fila;
+typedef struct no{
+ 
+    int valor;
+    struct no * prox;
+}NO;
+typedef struct pilha{
+  
+    NO * topo; 
+    int tam;
 
-typedef struct {
-    Fila q1;
-    Fila q2;
-} PilhaDeFilas;
+    struct pilha *prox;
+}PILHA;
 
-int fila_vazia(Fila *f) {
-    return f->frente == -1 && f->tras == -1;
+typedef struct fila{
+    PILHA * inicio;
+    PILHA * fim;
+    int tam;
+    struct fila *prox;
+}FILA;
+
+void add_fila(PILHA *p, FILA **f){
+    FILA *nova_fila = malloc(sizeof(FILA));
+    nova_fila->inicio = p;
+    nova_fila->fim = p;
+    nova_fila->tam = 1;
+    nova_fila->prox = *f;
+    *f = nova_fila;
 }
 
-int fila_cheia(Fila *f) {
-    return (f->tras + 1) % MAX_QUEUE_SIZE == f->frente;
+void add_pilha(int valor, PILHA *p){
+    NO * novo = malloc(sizeof(NO));
+    novo->valor = valor;
+    novo->prox = p->topo;
+    p->topo = novo;
+    p->tam++;
 }
 
-void fila_enfileirar(Fila *f, int val) {
-    if (fila_cheia(f)) {
-        printf("Estouro da Fila\n");
-        return;
+void imprimir_pilha(PILHA *p){
+    NO * aux = p->topo;
+    for(int i = 0; i<p->tam; i++){
+        printf("valor: %d\n", aux->valor);
+        aux = aux->prox;
     }
-    if (fila_vazia(f)) {
-        f->frente = f->tras = 0;
-    } else {
-        f->tras = (f->tras + 1) % MAX_QUEUE_SIZE;
-    }
-    f->dados[f->tras] = val;
 }
 
-int fila_desenfileirar(Fila *f) {
-    if (fila_vazia(f)) {
-        printf("Fila Vazia\n");
+void imprimir_fila(FILA *f){
+    int i = 0;
+    PILHA * aux_pilha;
+    while (f != NULL) {
+        aux_pilha = f->inicio;
+        printf("Fila %d:\n", i);
+        while (aux_pilha != NULL) {
+            imprimir_pilha(aux_pilha);
+            aux_pilha = aux_pilha->prox;
+        }
+        f = f->prox;
+        i++;
+    }
+}
+
+int remover_pilha(PILHA *p){
+    if(p->tam > 0){ 
+        NO *lixo = p->topo;
+        int valor = p->topo->valor;
+        p->topo = p->topo->prox;
+        free(lixo);
+        p->tam--;
+        return valor;
+    }else{
+        printf("Pilha vazia! \n :(");
         return -1;
     }
-    int val = f->dados[f->frente];
-    if (f->frente == f->tras) {
-        f->frente = f->tras = -1;
-    } else {
-        f->frente = (f->frente + 1) % MAX_QUEUE_SIZE;
-    }
-    return val;
 }
 
-void pilha_de_filas_empilhar(PilhaDeFilas *p, int val) {
-    fila_enfileirar(&p->q1, val);
+PILHA * inicia(){
+   PILHA * aux = malloc(sizeof(PILHA));
+   aux->topo = NULL;
+   aux->tam = 0;
+   return aux;
 }
 
-int pilha_de_filas_desempilhar(PilhaDeFilas *p) {
-    if (fila_vazia(&p->q1)) {
-        printf("Pilha Vazia\n");
-        return -1;
-    }
-    while (!fila_vazia(&p->q1) && p->q1.frente != p->q1.tras) {
-        int val = fila_desenfileirar(&p->q1);
-        fila_enfileirar(&p->q2, val);
-    }
-    int val = fila_desenfileirar(&p->q1);
-    Fila temp = p->q1;
-    p->q1 = p->q2;
-    p->q2 = temp;
-    return val;
+int main(){
+    PILHA *p1 = inicia();
+    add_pilha(18, p1);
+    add_pilha(19, p1);
+
+    PILHA *p2 = inicia();
+    add_pilha(17, p2);
+    add_pilha(28, p2);
+    add_pilha(39, p2);
+    add_pilha(45, p2);
+
+    FILA *fila_topo = NULL;
+    add_fila(p1, &fila_topo);
+    add_fila(p2, &fila_topo);
+
+    imprimir_fila(fila_topo);
+    return 0;
 }
